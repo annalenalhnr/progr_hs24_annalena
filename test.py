531073b2f7875bfa7574c4c8f473ad033488ac71
annalenalhnr <annalena.lehner@stud.hslu.ch>
2024-11-11 15:16:28 +0100

    Version 2

--- test.py
+++ test.py
@@ -1,2 +1,28 @@
-print("Heloo")
-print("hallo")
+import customtkinter as ctk
+
+def getEntry():
+    eingabe = entry.get()
+    print(eingabe)
+    label_entry.configure(text=eingabe)
+
+
+root = ctk.CTk()
+
+root.geometry("800x600")
+
+label = ctk.CTkLabel(master=root, text="Hello World", font=("arial", 18))
+label.pack(pady=10)
+
+entry = ctk.CTkLabel(master=root, width=200)
+entry.pack(pady=10)
+
+button = ctk.CTkButton(master=root, text="Click me", command=getEntry)
+button.pack(pady=10)
+
+label_entry = ctk.CTkLabel(master=root, text="", font=("Arial", 12))
+label_entry.place(relx=0.0, rely=1.0, y=-10, anchor="sw")
+
+root.mainloop()
+
+print(helloa)
+
